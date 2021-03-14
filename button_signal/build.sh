02479e0e65e70deff3ebc636fef0f1e626a1fc27
rgimad <rustem.gimadutdinov@gmail.com>
2021-03-14 23:18:29 +0300

    first commit

---
+++ button_signal/build.sh
@@ -0,0 +1,1 @@
+gcc `pkg-config --cflags gtk+-3.0` -o button_signal button_signal.c `pkg-config --libs gtk+-3.0`
---
+++ empty_window/build.sh
@@ -0,0 +1,1 @@
+gcc `pkg-config --cflags gtk+-3.0` -o empty_window empty_window.c `pkg-config --libs gtk+-3.0`
